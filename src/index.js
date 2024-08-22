9f24bcbfbe4cc7064a7223363d66f4080d37cae2
Shant Dashjian <shantgdashjian@gmail.com>
2024-08-22 15:08:24 +0400

    Add code to talk to Claude

--- src/index.js
+++ src/index.js
@@ -1,15 +1,35 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import Anthropic from '@anthropic-ai/sdk'
+
+const corsHeaders = {
+	'Access-Control-Allow-Origin': 'https://thedescriber.pages.dev',
+	'Access-Control-Allow-Methods': 'POST, OPTIONS',
+	'Access-Control-Allow-Headers': 'Content-Type',
+}
 
 export default {
 	async fetch(request, env, ctx) {
-		return new Response('Hello World!');
+		const anthropic = new Anthropic({
+			apiKey: env.ANTHROPIC_API_KEY,
+		})
+
+		if (request.method === 'OPTIONS') {
+			return new Response(null, {
+				status: 200,
+				headers: corsHeaders
+			})
+		}
+
+		try {
+			const messages = await request.json()
+			const msg = await anthropic.messages.create({
+				model: 'claude-3-5-sonnet-20240620',
+				max_tokens: 300,
+				system: 'You are an image describer. When asked to describe an image, provide an accurate description.',
+				messages: messages
+			})
+			return new Response(JSON.stringify(msg.content[0].text), { headers: corsHeaders })
+		} catch (error) {
+			return new Response(JSON.stringify({ error: error }), { status: 500, headers: corsHeaders })
+		}
 	},
-};
+}
